22c64791d174c4d0e123544d042e8a2c027cd72c
yzheng <1820510329@qq.com>
2016-07-06 15:17:36 +0800

    together

--- preload.js
+++ preload.js
@@ -13,6 +13,8 @@ var receive_flag = true
 var send_flag = true
 //是否添加信息
 var addMsg_flag = true
+//是否可以点击
+var click_falg = true
 // 应对 微信网页偷换了console 使起失效
 // 保住console引用 便于使用
 window._console = window.console
@@ -70,143 +72,123 @@ function onLogin(){
 					_console.log($reddot[r]);
 					var $chat_item = $reddot[r].closest('.chat_item')
 					msg_receive.push($chat_item);
-					_console.log("添加一条信息")
 					$chat_item.click()
-					_console.log(msg_receive.length)
-					_console.log(receive_flag)
 				}
 				reset()
 				addMsg_flag = true;
 			}
 		}
-	}, 511)
+	}, 100)
 	var send_msg = setInterval(function(){
 		//如果收到信息  则解析信息
 		if(msg_receive.length>0){
 			if(receive_flag){
 				receive_flag = false;
-				debug("开始解析信息",msg_receive.length)
-				for(var m in msg_receive){
-					debug("信息组里的数据",msg_receive[m])
-					resolve_qst(msg_receive[m],m)
-				}
+				resolve_qst(msg_receive[0])
+				msg_receive = msg_receive.slice(1)
 			}
 		}
 		//处理结果集里面的信息
+		_console.log("结果集长度",msg_send.length);
 		if(msg_send.length>0){
 			if(send_flag){
+				_console.log("开始发送信息",send_flag)
 				send_flag = false;
-				debug("发送数据",msg_send.length)
-				for(var m in msg_send){
-					// debug("结果组里的数据",msg_send[m])
-					resolve_asw(msg_send[m],m)
-
-				}
+				resolve_asw(msg_send[0])
+				msg_send = msg_send.slice(1)
 			}
 		}
 		// var newFriend = $(".chat_item .slide-left .ng-scope")[0];
-	}, 1770)
+	}, 2070)
 }
-function resolve_qst($chat_item,index){   
-	$chat_item.click()
-	setTimeout(function(){
-		_console.log("解析信息下标",index);
-		var reply = {}
-		var usernickname = $(".header .nickname [ng-bind-html='account.NickName']").text();
-		// 自动回复 相同的内容
-		var $msg = $([
-			'.message:not(.me) .bubble_cont > div',
-			'.message:not(.me) .bubble_cont > a.app',
-			'.message:not(.me) .emoticon',
-			'.message_system'
-		].join(', ')).last()
-		var $message = $msg.closest('.message')
-		var $nickname = $message.find('.nickname')
-		var $titlename = $('.title_name')
-		if ($nickname.length) { // 群聊
-			var from = $nickname.text()
-			var room = $titlename.text()
-		} else { // 单聊
-			var from = $titlename.text()
-			var room = null
-		}
-		debug('来自', from, room) // 这里的nickname会被remark覆盖
-		if ($msg.is('.card')) {
-			var name = $msg.find('.display_name').text()
-			var wxid = $msg.find('.signature').text()
-			var img = $msg.find('.img').prop('src') // 认证限制
-			debug('接收', 'card', name, wxid)
-			reply.text = name + '\n' + wxid
-			addFriends();
-			if(index==(msg_receive.length-1)){
-				msg_receive = [];
-				receive_flag = true;
-				reset();
+//解析信息
+function resolve_qst($chat_item){   
+	_console.log("解析信息",click_falg)
+	if(click_falg){
+		click_falg = false;
+		_console.log("禁止点击","解析")
+		$chat_item.click()
+		setTimeout(function(){
+			var reply = {}
+			var usernickname = $(".header .nickname [ng-bind-html='account.NickName']").text();
+			// 自动回复 相同的内容
+			var $msg = $([
+				'.message:not(.me) .bubble_cont > div',
+				'.message:not(.me) .bubble_cont > a.app',
+				'.message:not(.me) .emoticon',
+				'.message_system'
+			].join(', ')).last()
+			var $message = $msg.closest('.message')
+			var $nickname = $message.find('.nickname')
+			var $titlename = $('.title_name')
+			if ($nickname.length) { // 群聊
+				var from = $nickname.text()
+				var room = $titlename.text()
+			} else { // 单聊
+				var from = $titlename.text()
+				var room = null
 			}
-			return false;
-		} else if ($msg.is('a.app')) {
-			var url = $msg.attr('href')
-			url = decodeURIComponent(url.match(/requrl=(.+?)&/)[1])
-			var title = $msg.find('.title').text()
-			var desc = $msg.find('.desc').text()
-			var img = $msg.find('.cover').prop('src') // 认证限制
-			debug('接收', 'link', title, desc, url)
-			reply.text = title + '\n' + url
-		} else if ($msg.is('.plain')) {
-			var text = ''
-			var $text = $msg.find('.js_message_plain')
-			text = $text.text();
-			//判断消息是否是数字或链接
-			// if(text.indexOf("http://")>=0||text.indexOf("https://")>=0||!isNaN(text)){
-			// 	reply.text = text
-			// }else{
-			// 	debug('接收','不合格信息(沉默)',text)
-			// 	return false
-			// }
-			reply.text = text
-			debug('接收', 'text', text)
-		}else{
-			debug('接收', 'BUG', $msg);
-			if(index==(msg_receive.length-1)){
-				msg_receive = [];
-				receive_flag = true;
-				reset();
+			debug('来自', from) // 这里的nickname会被remark覆盖
+			if ($msg.is('.card')) {
+				var name = $msg.find('.display_name').text()
+				var wxid = $msg.find('.signature').text()
+				var img = $msg.find('.img').prop('src') // 认证限制
+				debug('接收', 'card', name, wxid)
+				reply.text = ""
+				addFriends();
+			} else if ($msg.is('a.app')) {
+				var url = $msg.attr('href')
+				url = decodeURIComponent(url.match(/requrl=(.+?)&/)[1])
+				var title = $msg.find('.title').text()
+				var desc = $msg.find('.desc').text()
+				var img = $msg.find('.cover').prop('src') // 认证限制
+				debug('接收', 'link', title, desc, url)
+				reply.text = title + '\n' + url
+			} else if ($msg.is('.plain')) {
+				var text = ''
+				var $text = $msg.find('.js_message_plain')
+				text = $text.text();
+				//判断消息是否是数字或链接
+				// if(text.indexOf("http://")>=0||text.indexOf("https://")>=0||!isNaN(text)){
+				// 	reply.text = text
+				// }else{
+				// 	debug('接收','不合格信息(沉默)',text)
+				// 	return false
+				// }
+				reply.text = text
+				debug('接收', 'text', text)
+			}else{
+				debug('接收', 'BUG', $msg);
 			}
-			return false;
-		}
-		debug('回复', reply)
-		requestData(reply.text,$titlename.text(),$chat_item)
-		if(index==(msg_receive.length-1)){
-			msg_receive = [];
-			receive_flag = true;
+			requestData(reply.text,$titlename.text(),$chat_item)
 			reset();
-		}
-		return reply.text;
-	}, 100)
+			receive_flag = true;
+		}, 100)
+	}else{
+		msg_receive.push($chat_item)
+		receive_flag = true
+	}
+	
 }
 //处理结果集   发送数据
-function resolve_asw(data_item,index){
-	// debug("发送数据到",data_item.item,"发送内容",data_item.text);
-	_console.log("发送数据",index)
-	_console.log(data_item.item)
-	setTimeout(function(){
+function resolve_asw(data_item){
+	_console.log("发送数据",click_falg)
+	if(click_falg){
+		_console.log("禁止点击","发送")
+		click_falg = false;
 		data_item.item.click();
 		setTimeout(function(){
 			var opt = {};
 			opt.html = data_item.text;
-			_console.log("回复内容",opt.html)
 			paste(opt);
 			$('.btn_send')[0].click();
-			if(index==(msg_send.length-1)){
-				msg_send = [];
-				send_flag = true;
-			}
-			// todo click点击三次
 			reset();
+			send_flag = true;
 		},100)
-		
-	},1000*index);
-	
+	}else{
+		msg_send.push(data_item)
+		send_flag = true
+	}
 }
 
 function reset(){
@@ -214,6 +196,8 @@ function reset(){
 	var msgs = $('#chatArea').scope().chatContent
 	if (msgs.length >= 30) msgs.splice(0, 20)
 	$('img[src*=filehelper]').closest('.chat_item')[0].click()
+	click_falg = true;
+	_console.log("是否可以点击",click_falg)
 }
 
 function paste(opt){
@@ -221,15 +205,6 @@ function paste(opt){
 	var oldHtml = clipboard.readHtml()
 	var oldText = clipboard.readText()
 	clipboard.clear() // 必须清空
-	if (opt.image) {
-		// 不知为啥 linux上 clipboard+nativeimage无效
-		try {
-			clipboard.writeImage(NativeImage.createFromPath(opt.image))
-		} catch (err) {
-			opt.image = null
-			opt.text = '发不出图片'
-		}
-	}
 	if (opt.html) clipboard.writeHtml(opt.html)
 	if (opt.text) clipboard.writeText(opt.text)
 	$('#editArea')[0].focus()
@@ -241,7 +216,6 @@ function paste(opt){
 
 //添加好友
 function addFriends(){
-	_console.log("开始自动添加好友。。。。。");
 	$(".bubble").filter(".js_message_bubble").filter(".ng-scope").filter(".bubble_default").filter(".left").children(".bubble_cont").children(".card")[0].click();
 	$("#mmpop_profile .nickname_area .web_wechat_tab_add").click();
 	$("#mmpop_profile .form_area a.button").click();
@@ -262,7 +236,6 @@ function requestData(urlStr,nickname,chat_item){
 	var url = '';
 	var uStr = urlStr;
 	var reply = {};
-	debug("接收内容",urlStr)
 	if(!isNaN(uStr)){
 		var lists = JSON.parse(storage.getItem(nickname));
 		if(lists&&parseInt(uStr)<=lists.length&&lists.length>0){
@@ -290,7 +263,7 @@ function requestData(urlStr,nickname,chat_item){
 		if(history&&history.length>0){
 			reply.html = "";
 			for(var h in history){
-				debug(history[h],"新旧标题",title,"结果",history[h].title==title)
+				// debug(history[h],"新旧标题",title,"结果",history[h].title==title)
 				if(history[h].title==title){
 					var beginNo = parseInt(history[h].begin),endNo = parseInt(history[h].end);
 					var datalist = JSON.parse(storage.getItem(nickname));
@@ -338,8 +311,20 @@ function dataConn(requestUrl,title,url,nickname,chat_item){
         success: function(res_data, textStatus, XMLHttpRequest){
             var reply = {};
             var data = res_data.searchItems;
+            var tags = res_data.tags;
             reply.html = '';
-            if(data.length>0){
+            if(tags&&tags.length>0){
+            	for(var t in tags){
+            		reply.html += tags[t] + " | " 
+            	}
+				reply.html += "<br>" 
+				var tag_l = reply.html.length;
+				for(var r =4;r<tag_l*1.5;r++){
+					reply.html += "-"
+				} 
+				reply.html += "<br>" 
+            }
+            if(data&&data.length>0){
             	var new_item = [];
             	var new_item_title = JSON.parse(storage.getItem(nickname+"_send"))?JSON.parse(storage.getItem(nickname+"_send")):[]
 				var old_item = JSON.parse(storage.getItem(nickname))?JSON.parse(storage.getItem(nickname)):[];
